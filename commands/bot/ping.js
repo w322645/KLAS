794a2814295f886b30feefa3792e134741023785
Glitch (regal-imported-armadillo) <none>
2021-06-15 12:18:53 +0000

    🗼🏙 Checkpoint ./commands/bot/ping.js:28679123/5643

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -1,56 +1,26 @@
-const Discord = require("discord.js");
-const data = require("quick.db");
-const talkedRecently = new Set();
+const Discord = require('discord.js');
+const db = require('quick.db');
 
+const moment = require('moment');
+require('moment-duration-format');
 exports.run = async (client, message, args) => {
-   if (talkedRecently.has(message.author.id)) {
-           return message.channel.send(new Discord.MessageEmbed().setColor('#36393f').setTitle('UYARI !').setDescription(`\`5\` Saniye de Bir Kullanabilirsin - <@!${message.author.id}>`))
-     .then(x => {x.delete({timeout: 3000})})
-    } else {
+   var olcum = await message.channel.send( ' Ölçüm yapılıyor, lütfen bekleyiniz...');
+ var sonuc = await message.channel.send( " Veriler alındı...").then(msg => msg.delete(3000))
+     await olcum.edit( ` **Tepki Gecikmesi** \`${Math.round((sonuc.createdTimestamp - olcum.createdTimestamp - client.ws.ping) )}\`**ms**\n **Bot Gecikmesi** \`${Math.round(client.ws.ping)}\`**ms**`);
+///
+  
+  
+}
 
-           // the user can type the command ... your command code goes here :)
-
-        // Adds the user to the set so that they can't talk for a minute
-        talkedRecently.add(message.author.id);
-        setTimeout(() => {
-        message.delete();
-          // Removes the user from the set after a minute
-          talkedRecently.delete(message.author.id);
-        }, 5000);// Şuan 5 Saniyedir Değiştirebilirsiniz.
-    }
-
-  Array.prototype.random = function() {
-    return this[Math.floor(Math.random() * this.length)];
-  };
-  message.channel
-    .send(
-      new Discord.MessageEmbed()
-        .setColor("#36393f")
-        .setDescription(
-          `${message.author} **Ping Hesaplanıyor... <a:ttnet:764635922917621791>**`
-        )
-    )
-    .then(m => {
-      setTimeout(() => {
-        m.edit(
-          new Discord.MessageEmbed()
-            .setColor("#36393f")
-            .addField(
-              "**__Gecikme Sürem__**",
-              `<a:7879_sinal:814581717553381398> **${client.ws.ping}** ms Olarak Hesaplandı.`,
-              true
-            )
-        );
-      }, 7500);
-    });
-};
 exports.conf = {
   enabled: true,
-  guildOnly: true,
-  aliases: ["p", "ms", "geçikme"],
+  guildOnly: false,
+  aliases: [],
   permLevel: 0
 };
 
 exports.help = {
-  name: "ping"
+  name: 'ping',
+  description: 'Ping was here',
+  usage: ''
 };
