48fab7254bf1f6ef8bde42a6d2f3f983561eaa02
Glitch (regal-imported-armadillo) <none>
2021-06-15 12:41:49 +0000

    🎟🏢 Checkpoint ./commands/bot/ping.js:28679123/1620

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const db = require('quick.db');
-
+var renk = ('#36393f')
 const moment = require('moment');
 require('moment-duration-format');
 exports.run = async (client, message, args) => {
-   var olcum = await message.channel.send( ' Ölçüm yapılıyor, lütfen bekleyiniz...');
-   var sonuc = await message.channel.send( " Veriler alındı...")
-     await olcum.edit( ` **Tepki Gecikmesi** \`${Math.round((sonuc.createdTimestamp - olcum.createdTimestamp - client.ws.ping) )}\`**ms**\n **Bot Gecikmesi** \`${Math.round(client.ws.ping)}\`**ms**`);
+   var olcum = await message.channel.send(new Discord.MessageEmbed().setColor(renk).setDescription(' Ölçüm yapılıyor, lütfen bekleyiniz...'));
+   var sonuc = await message.channel.send((`\`\`\`fix\nVeriler alındı...\n\`\`\``))
+     await olcum.edit(new Discord.MessageEmbed().setTitle('ping').setColor(renk).setDescription(` **Tepki Gecikmesi** \`${Math.round((sonuc.createdTimestamp - olcum.createdTimestamp - client.ws.ping) )}\`**ms**\n **Bot Gecikmesi** \`${Math.round(client.ws.ping)}\`**ms**`));
   ///
   
   
