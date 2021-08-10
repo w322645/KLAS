f6851714398e9f77c949668bebfbae295e05cc8b
Glitch (purple-gold-pie) <none>
2021-08-10 14:10:08 +0000

    🌋🛋 Checkpoint ./commands/bot/dm-mesaj.js:33847415/5268 ./commands/yardÄ±m/menÃ¼sahip.js:33847415/109

--- commands/bot/dm-mesaj.js
+++ commands/bot/dm-mesaj.js
@@ -1,22 +1,21 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
- 
+  if (message.author.id != "852641223947845743") return message.reply('Bunu sadece botun sahibi kullanabilir'); // Bu kısmı silerseniz yetkili olan herkes kullanabilir. silmenizi tavsiye etmem.
     
   if (!message.guild) {
-  const ozelmesajuyari = new Discord.RichEmbed()
+  const ozelmesajuyari = new Discord.MessageEmbed()
   .setColor(0xFF0000)
   .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
+  .setAuthor(message.author.username, message.author.avatarURL())
   .addField('⚠ Uyarı ⚠', 'Bu komutu özel mesajlarda kullanamazsın.');
-  return message.author.sendEmbed(ozelmesajuyari); }
+  return message.author.send(ozelmesajuyari); }
   let guild = message.guild;
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
   if (reason.length < 1) return message.reply('Ne yazmam gerek onuda söyle.');
-  if (message.mentions.users.size < 1) return message.reply('Ama böyle olmaz kime'de mesaj atacağımı yazman gerek.').catch(console.error);
-  message.delete();
-  message.reply('Mesaj'ını ilettim kanka.')
-  const embed = new Discord.RichEmbed()
+  if (message.mentions.users.cache.size < 1) return message.reply('Ama böyle olmaz kime  mesaj atacağımı yazman gerek.').catch(console.error);
+  message.reply('Mesajını ilettim kanka.')
+  const embed = new Discord.MessageEmbed()
   .setColor('RANDOM')
   .setTitle(`**Huhu orda mısın bir mesajın var ^^ **`)
   .setTimestamp()
