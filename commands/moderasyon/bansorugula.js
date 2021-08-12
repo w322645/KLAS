975c8a4f0002dfbda819907bb088840880c26463
Glitch (purple-gold-pie) <none>
2021-08-12 08:47:01 +0000

    👾🎷 Checkpoint ./commands/moderasyon/bansorugula.js:33847415/536

--- commands/moderasyon/bansorugula.js
+++ commands/moderasyon/bansorugula.js
@@ -1,19 +1,7 @@
 const Discord = require('discord.js');
 const db = require('quick.db')
-  const talkedRecently = new Set();
 exports.run = (client, message, args) => {
 
-
- if (talkedRecently.has(message.author.id)) {
-           return message.channel.send(new Discord.MessageEmbed().setColor('#36393f').setTitle('UYARI !').setDescription(`\`5\` Saniye de Bir Kullanabilirsin - <@!${message.author.id}>`))
-     .then(x => {x.delete({timeout: 3000})})
-    } else {
-
-           // the user can type the command ... your command code goes here :)
-
-        // Adds the user to the set so that they can't talk for a minute
-        talkedRecently.add(message.author.id);
-    }
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(new Discord.MessageEmbed().setColor("#36393f").setDescription(":no_entry: Yeterli yetkiniz yok"));
     let kullanici = args[0];
     if (!kullanici) return message.channel.send(new Discord.MessageEmbed().setImage("https://s3.gifyu.com/images/ezgif.com-gif-maker-17ce77b310641726e.gif").setColor("#36393f").setDescription('**<:okeys:820285379298328607> Banını Sorgulayacağım Kişinin İd ini Girmen Gerek ** \n\n Nasıl İd Alındığını Bilmiyosan Buyur ⬇️ '));
