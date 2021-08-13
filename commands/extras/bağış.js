cff08ea8eec578da58bc27b87bd65c4bfbd29548
Glitch (purple-gold-pie) <none>
2021-08-13 17:23:11 +0000

    🐟📠 Checkpoint ./commands/extras/bağış.js:33847415/634

--- commands/extras/bağış.js
+++ commands/extras/bağış.js
@@ -3,16 +3,19 @@ const db = require('quick.db');
 
 exports.run = async (client, message, args) => {
 
-	const kıllıam = new Discord.MessageEmbed()
+  var renk = '#36393f'
+  const kıllıam = new Discord.MessageEmbed()
 	.setTitle("`EFDA BAĞIŞ`")
-	.setDescription('\`\`\`bashEfda Bota Bağış Yapmak İçin Aşağıdaki Yerler Kullanabilir Ve Bize Maddi Açıdan Destek Olabilirsiniz. \n Bu Davranışınız İçin Sizlere Minnetarız Çok Teşekkür Ederiz \n Unutmayınız Siz Yoksanız Biz Bir Hiç Siz Varsanız Biz Sizin İçin Varız :)\`\`\` \n\n **EFDA Sunar**')
-	.addField(`Papara İban`,`Yakında Eklenicek`)
+  .setColor(renk)
+  .addField(`Papara İban`,`Yakında Eklenicek`)
 	.addField(`İninal İban`,`Yakında Eklenicek`)
 	.addField(`Akbank İban`,`Yakında Eklenicek`)
 	.addField(`Diğer Kartlar İban`,`Daha Sonra Yakında Eklenicek`)
+  .addField(`Diğer Kartlar İban`,`Daha Sonra Yakında Eklenicek`)
 	.addField(`Diğer Kartlar İban`,`Daha Sonra Yakında Eklenicek`)
 	.addField(`Diğer Kartlar İban`,`Daha Sonra Yakında Eklenicek`)
 	.addField(`Diğer Kartlar İban`,`Daha Sonra Yakında Eklenicek`)
+  .setImage('https://cdn.glitch.com/478cc1e8-9c56-49a4-b910-3247fb1894aa%2Fyeni2.png?v=1621792931950')
 	message.channel.send(kıllıam);
 };
 
