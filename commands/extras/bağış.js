6e9bb3aac43a02cdd2c6d71de884f95772bc2f57
Glitch (purple-gold-pie) <none>
2021-08-13 17:34:17 +0000

    🍢🏏 Checkpoint ./commands/extras/bağış.js:33847415/318

--- commands/extras/bağış.js
+++ commands/extras/bağış.js
@@ -16,6 +16,7 @@ exports.run = async (client, message, args) => {
 	.addField(`Diğer Kartlar İban`,`Daha Sonra Yakında Eklenicek`)
 	.addField(`Diğer Kartlar İban`,`Daha Sonra Yakında Eklenicek`)
   .setImage('https://cdn.glitch.com/478cc1e8-9c56-49a4-b910-3247fb1894aa%2Fyeni2.png?v=1621792931950')
+  .setDescription('\`\`\`bash\n"Efda Bota Bağış Yapmak İçin Aşağıdaki Yerler Kullanabilir Ve Bize Maddi Açıdan Destek Olabilirsiniz. \n Bu Davranışınız İçin Sizlere Minnetarız Çok Teşekkür Ederiz \n Unutmayınız Siz Yoksanız Biz Bir Hiç Siz Varsanız Biz Sizin İçin Varız :)"\n\`\`\` \n\n **EFDA Sunar**',"Teşekkürler")
 	message.channel.send(kıllıam);
 };
 
