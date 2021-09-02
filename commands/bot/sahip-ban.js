c7f0b1f93672c2a787db018fdfd5a17da7ef2121
Glitch (purple-gold-pie) <none>
2021-09-02 10:53:41 +0000

    🍞🏌 Checkpoint ./commands/bot/sahip-ban.js:33847415/4342 ./commands/yardÄ±m/menÃ¼sahip.js:33847415/171

--- commands/bot/sahip-ban.js
+++ commands/bot/sahip-ban.js
@@ -1,66 +1,41 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
-  const talkedRecently = new Set();
-exports.run = async (client, message, args) => {
+const Discord = require('discord.js')
 
+    exports.run = (client, message, args) => {
+        let kullanici = message.mentions.members.first();
+        let sebep = args.slice(1).join(' ');
+        
+        if(!kullanici){
+            const kullanicihata = new Discord.MessageEmbed()
+            .setDescription(`${message.author} **Banlanacak kişiyi etiketle...**`)
+            .setColor('#ff0000')
+            return message.channel.send(kullanicihata)
+        }
+        if(!sebep){
+            const sebephata = new Discord.MessageEmbed()
+            .setDescription(`${message.author} **Tamam banlayacağım ama, sebep girmezsen kıyamamki buna.**`)
+            .setColor('#ff0000')
+            return message.channel.send(sebephata)
+        }
 
-if(!message.author.id === "758672982146809867") return message.reply("bunu sadece sahibim kullanabilir")
-    return message.channel.send("BAN Yetkisine sahip değilsin.");
+        if(kullanici && sebep){
+            kullanici.ban({reason: sebep})
 
-  let user = message.mentions.users.first();
-  let sebep = args.slice(1).join(" ") || "Belirtilmemiş.";
-  if (!user) return message.channel.send(" ``Bir kişi etiketlemelisin.``");
-  if (user.id === message.author.id)
-    return message.channel.send(" ``Kendini banlayamazsın.``");
-  if (user.id === client.user.id)
-    return message.channel.send(" ``Botu banlayamazsın.``");
-  if (user.id === message.guild.ownerID)
-    return message.channel.send(" ``Sunucu sahibini banlayamazsın.``");
-  if (!message.guild.member(user).bannable)
-    return message.reply(
-      "``Bu kişinin rolü senden üstte veya `Üyeleri yasakla` yetkisine sahip.``"
-    );
+            const ban =  new Discord.MessageEmbed()
+            .setDescription(`${kullanici} Kullanıcısı ${message.author} Tarafından **${sebep}** Sebebiyle Sunucudan Yasaklandı...!`)
+            .setAuthor(`${message.author.username} - İyi iş çıkardın dostum`, message.author.avatarURL({dynamic: true}))
+            .setColor('RANDOM');
+            message.channel.send(ban)
+        }
 
-  message.channel
-    .send(
-      "<@" +
-        user.id +
-        "> Kişisini **" +
-        sebep +
-        "** Sebebiyle banlamak istediğine eminmisin ? <a:4225_ban_button:814532928863731733>"
-    )
-    .then(async m => {
-      m.react("✅").then(r => {
-        const tamam = (reaction, user) =>
-          reaction.emoji.name == "✅" && user.id == message.author.id;
-        const tamam2 = m.createReactionCollector(tamam);
-
-        tamam2.on("collect", async r => {
-          message.guild.members.cache.get(user.id).ban({
-            reason: `${sebep}`
-          });
-        });
-      });
-      await m.react("❌").then(r => {
-        const tamam = (reaction, user) =>
-          reaction.emoji.name == "❌" && user.id == message.author.id;
-        const tamam2 = m.createReactionCollector(tamam);
-
-        tamam2.on("collect", async r => {
-          m.delete();
-          message.channel.send("Banlama işlemi iptal edildi.");
-        });
-      });
-    });
-};
+    }
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permlevel: 4
-};
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 4
+}
 
 exports.help = {
-  name: "ban"
-};
+    name: 'sahip-ban'
+}
