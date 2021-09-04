90c6e3103aef220d56e02f5ca9a574dcfae2e503
Glitch (purple-gold-pie) <none>
2021-09-04 05:35:58 +0000

    🎊🐡 Checkpoint ./commands/extras/google.js:33847415/2687 ./commands/yardÄ±m/menÃ¼3.js:33847415/173 ./commands/extras/wikipedia.js:33847415/1310

---
+++ commands/extras/wikipedia.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const superagent = require("superagent");
+const snekfetch = require("snekfetch");
+exports.run = async (client, message, args) => {
+  const query = args.join(" ");
+   const problem = args.join(" ");
+  const { body } = await snekfetch
+    .get("https://tr.wikipedia.org/w/api.php")
+    .query({
+      action: "query",
+      prop: "extracts",
+      format: "json",
+      titles: query,
+      exintro: "",
+      explaintext: "",
+      redirects: "",
+      formatversion: 2
+    });
+  if(problem) return message.channel.send('Birşey Y')
+  if (body.query.pages[0].missing) return message.channel.send("Maalesef sonuç bulunamadı.");
+  const devtr = new Discord.MessageEmbed()
+    .setColor(0x00a2e8)
+    .setAuthor('EFDA')
+    .setTitle(body.query.pages[0].title)
+    .setAuthor("Vikipedi", "https://i.imgur.com/a4eeEhh.png")
+    .setDescription(
+      body.query.pages[0].extract.substr(0, 2000).replace(/[\n]/g, "\n\n")
+    );
+  return message.channel.send(devtr).catch(console.error);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["vikipedi", "viki"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "wikipedia",
+  description: "Wikipediadan bilgi verir",
+  usage: "wikipedia <kelime>"
+};
