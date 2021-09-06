a52d8c98bdd7db28989b04b57cd57131839d5ab5
Glitch (purple-gold-pie) <none>
2021-09-06 16:20:03 +0000

    🛳💑 Checkpoint ./commands/moderasyon/yavaÅŸmod.js:33847415/3479

--- commands/moderasyon/yavaÅŸmod.js
+++ commands/moderasyon/yavaÅŸmod.js
@@ -1,22 +1,30 @@
 const Discord = require('discord.js');
 
 exports.run = async(client, msg, args) => {
-if (msg.channel.type !== "text") return;
+var renk =('#36393f')
+
+  if (msg.channel.type !== "text") return;
   
 const limit = args[0];
   
   if(!limit) {
               var embed = new Discord.MessageEmbed()
                 .setDescription(`Yazma sınırını (süresini) kaç saniye olarak ayarlamak istediğinizi yazınız!`)
-              .setColor("#36393f")
+              .setColor(renk)
             msg.channel.send({embed: embed})
             return
           }
-  
+if(limit > 0) {
+  var a = new Discord.MessageEmbed()
+  .setDescription("Yavaş Mod Başarılı Bir Şekilde Kapatılmıştır")
+  .setColor(renk)
+  msg.channel.send({emded: a});
+   return
+}
 if (isNaN(limit)) {
   var s = new Discord.MessageEmbed()
   .setDescription("Yazma sınırını (süresini) kaç saniye olarak ayarlamak istediğinizi yazınız!")
-  .setColor("#36393f")
+  .setColor(renk)
   msg.channel.send({embed: s});
     return
 }
@@ -24,14 +32,14 @@ if (isNaN(limit)) {
 if (limit > 21600) {
   var x = new Discord.MessageEmbed()
   .setDescription("Yazma sınırı (süresi) limiti maksimum **21600** saniye olabilir!")
-  .setColor("#36393f")
+  .setColor(renk)
   msg.channel.send({embed: x});
     return
 }
     var e = new Discord.MessageEmbed()
     .setDescription(`Yazma sınırı (süresi) başarıyla **${limit}** saniye olarak ayarlanmıştır!`)
     .setThumbnail(`https://dummyimage.com/500x500/2f3136f/f2f2f2&text=${limit}%20Saniye`)
-    .setColor("#36393f")
+    .setColor(renk)
     msg.channel.send({embed: e})
   
 var request = require('request');
