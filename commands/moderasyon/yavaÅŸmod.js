269c2df9ab1bc9760f659ade0fe5f6865af82810
Glitch (purple-gold-pie) <none>
2021-09-06 15:56:57 +0000

    🌺🍐 Checkpoint ./commands/moderasyon/yavaÅŸmod.js:33847415/3192

--- commands/moderasyon/yavaÅŸmod.js
+++ commands/moderasyon/yavaÅŸmod.js
@@ -8,7 +8,7 @@ const limit = args[0];
   if(!limit) {
               var embed = new Discord.MessageEmbed()
                 .setDescription(`Yazma sınırını (süresini) kaç saniye olarak ayarlamak istediğinizi yazınız!`)
-              .setColor("RANDOM")
+              .setColor("#36393f")
             msg.channel.send({embed: embed})
             return
           }
@@ -16,21 +16,22 @@ const limit = args[0];
 if (isNaN(limit)) {
   var s = new Discord.MessageEmbed()
   .setDescription("Yazma sınırını (süresini) kaç saniye olarak ayarlamak istediğinizi yazınız!")
-  .setColor("RANDOM")
+  .setColor("#36393f")
   msg.channel.send({embed: s});
     return
 }
   
-if (limit > 10) {
+if (limit > 120) {
   var x = new Discord.MessageEmbed()
-  .setDescription("Yazma sınırı (süresi) limiti maksimum **10** saniye olabilir!")
-  .setColor("RANDOM")
+  .setDescription("Yazma sınırı (süresi) limiti maksimum **120** saniye olabilir!")
+  .setColor("#36393f")
   msg.channel.send({embed: x});
     return
 }
     var e = new Discord.MessageEmbed()
     .setDescription(`Yazma sınırı (süresi) başarıyla **${limit}** saniye olarak ayarlanmıştır!`)
-    .setColor("RANDOM")
+    .setThumbnail(`https://dummyimage.com/140x140/2f3136f/f2f2f2&text=Saniye%20${limit}`)
+    .setColor("#36393f")
     msg.channel.send({embed: e})
   
 var request = require('request');
