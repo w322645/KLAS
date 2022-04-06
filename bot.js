a4550997a1cdb4ea0feb5c1af7ff2637193f4b7d
Glitch (purple-gold-pie) <none>
2022-04-06 08:41:17 +0000

    🎊💁 Checkpoint ./bot.js:33847415/3490 ./package.json:33847415/4356

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ const db = require("quick.db");
 const moment = require("moment");
 const Canvas = require("canvas");
 const disbut = require("discord-buttons");
+const express = require('express');
+const http = require('http')
 disbut(client);
 /////TABLES
 var commandtable = new AsciiTable("EFDA BOT KOMUT MASASI");
@@ -1198,5 +1200,16 @@ client.on("guildMemberRemove", (member) => {
         )
     );
 });
+//BOT AKTİFLİK ŞEYSİ
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping tamamdır.");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://dccs.glitch.me/`);
+}, 28000);
+
 
 client.login(process.env.sebastian);
